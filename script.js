0276604f13cba6b91d1be60a73f296be922bad52
emmeline.heitzler <emmeline.heitzler@epita.fr>
2023-05-20 11:05:57 +0900

    Main page ok"

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ function showList() {
     console.log("showList start");
     fetch("ingredient.json")
         .then(response => response.json())
-        .then(data => createList(data));
+        .then(data => createList(data))
+
 }
 
 function createList(data) {
     const mainUL = document.createElement('fieldset');
     const legendMainUL = document.createElement('legend');
-    legendMainUL.innerHTML = "Choose your ingredient";
+    legendMainUL.innerHTML = "Choose your ingredients:";
     mainUL.appendChild(legendMainUL);
     for (let i = 0; i < data.ingredients.length; i++) {
         const bigDiv = document.createElement('div');
-        bigDiv.className = data.ingredients[i].type;
+        bigDiv.id = data.ingredients[i].type;
+        bigDiv.className = "ingredient"
         const collapsButton = document.createElement('button');
         collapsButton.type = 'button';
         collapsButton.className = "collapsible";
+        collapsButton.id = data.ingredients[i].type;
         collapsButton.innerHTML = data.ingredients[i].type;
 
         const divButton = document.createElement('div');
-        divButton.className = "div-button";
+        divButton.className = "div-button";//Content of collapsible 
         divButton.id = data.ingredients[i].type + "-button";
         const contentUL = document.createElement('div');
         for (var key in data.ingredients[i].content) {
@@ -46,4 +49,41 @@ function createList(data) {
     submitButton.innerHTML = "Let's cook!";
     mainUL.appendChild(submitButton);
     document.body.appendChild(mainUL);
+
+    createCollapsible();
+}
+
+function createCollapsible() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    console.log("coll =" + coll.length);
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                //content.style.display = "none";
+                content.style.maxHeight = null;
+            } else {
+                //content.style.display = "inline-grid";
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+            closeCollapsible(content.id);
+        });
+    }
+}
+function closeCollapsible(content) {
+    var coll = document.getElementsByClassName("collapsible active");
+    var i;
+    console.log("content : " + content);
+    for (i = 0; i < coll.length; i++) {
+        console.log("coll[i].id : " + coll[i].id);
+        var contentToClose = coll[i].nextElementSibling;
+        if (contentToClose.id !== content) {
+            console.log("contentToClose id = " + contentToClose.id);
+            //contentToClose.style.display = "none";
+            contentToClose.style.maxHeight = null;
+            coll[i].className = "collapsible";
+        }
+    }
 }
