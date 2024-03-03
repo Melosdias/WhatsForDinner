f21c1df68871cdf8a94b81f0c0bfae88c1d96b73
Emmeline <97628233+Melosdias@users.noreply.github.com>
2024-03-03 13:33:46 +0100

    Update mainPage.php

--- pages/mainPage.php
+++ pages/mainPage.php
@@ -14,7 +14,7 @@ if (!isset($_COOKIE["user"])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>What do we eat tonight ?</title>
+    <title>What's for dinner?</title>
     <link rel="stylesheet" type="text/css" href="mainPage.css">
     <link rel="shortcut icon" href="../imageWebsite/logov2.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
